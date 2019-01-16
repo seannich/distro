d541dee5d162546820fb613a0a4e96361db17ae3
Joel Tio <joeltiojinhon@gmail.com>
2019-01-16 10:41:08 +0800

    Update getProjectConfig to return JavaSciprt object

--- src/webRPC/getProjectConfig.js
+++ src/webRPC/getProjectConfig.js
@@ -1,18 +1,22 @@
 import axios from 'axios';
+import parser from 'fast-xml-parser';
 
 const PROJECT_CONFIG_SUFFIX = 'get_project_config.php';
 
-function toCorsProxyURL(URL) {
+function toCORSProxyURL(URL) {
     return `https://cors.io/?${URL}`;
 }
 
 function getProjectConfig(projectURL) {
-    const url = toCorsProxyURL(`${projectURL}${PROJECT_CONFIG_SUFFIX}`);
+    const url = toCORSProxyURL(`${projectURL}${PROJECT_CONFIG_SUFFIX}`);
+
     return axios.get(url, {
         crossDomain: true,
     }).then(response => new Promise((resolve, reject) => {
         if (response.status === 200) {
-            resolve(response.data);
+            // The response data is XML. Parse the XML into a JavaScript object
+            const data = parser.parse(response.data);
+            resolve(data);
         } else {
             reject(response);
         }
