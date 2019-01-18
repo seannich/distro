f65544a9d22e84d8f6b8341eae607f09282fbdeb
Joel Tio <joeltiojinhon@gmail.com>
2019-01-18 09:49:28 +0800

    Move webRPC account functions to another file

---
+++ src/webRPC/account.js
@@ -0,0 +1,76 @@
+import md5 from 'md5';
+
+import { fetchWebRPC } from './webRPC';
+import {
+    CREATE_ACCOUNT_SUFFIX, LOOKUP_ACCOUNT_SUFFIX,
+    GET_ACCOUNT_INFO_SUFFIX, SET_ACCOUNT_INFO_SUFFIX,
+    SET_HOST_INFO_SUFFIX, SHOW_USER_SUFFIX,
+} from './settings';
+
+function createPasswdHash(password, email) {
+    // As documented in: https://boinc.berkeley.edu/trac/wiki/PasswordHash
+    return md5(password + email);
+}
+
+export function createAccount(projectURL, emailAddress, password, userName, options) {
+    const passwdHash = createPasswdHash(password, emailAddress);
+
+    const endpoint = `${projectURL}${CREATE_ACCOUNT_SUFFIX}`;
+    const params = {
+        email_addr: emailAddress,
+        passwd_hash: passwdHash,
+        user_name: userName,
+        ...options,
+    };
+
+    return fetchWebRPC(endpoint, params);
+}
+
+export function lookupAccount(projectURL, options) {
+    const endpoint = `${projectURL}${LOOKUP_ACCOUNT_SUFFIX}`;
+    return fetchWebRPC(endpoint, options);
+}
+
+export function getAccountInfo(projectURL, accountKey, opaqueAuth) {
+    const endpoint = `${projectURL}${GET_ACCOUNT_INFO_SUFFIX}`;
+    const params = {
+        account_key: accountKey,
+        opaque_auth: opaqueAuth,
+    };
+
+    return fetchWebRPC(endpoint, params);
+}
+
+export function setAccountInfo(projectURL, accountKey, opaqueAuth, options) {
+    const endpoint = `${projectURL}${SET_ACCOUNT_INFO_SUFFIX}`;
+    const params = {
+        account_key: accountKey,
+        opaque_auth: opaqueAuth,
+        ...options,
+    };
+
+    return fetchWebRPC(endpoint, params);
+}
+
+export function setHostInfo(projectURL, accountKey, opaqueAuth, hostID, venue) {
+    const endpoint = `${projectURL}${SET_HOST_INFO_SUFFIX}`;
+    const params = {
+        account_key: accountKey,
+        opaque_auth: opaqueAuth,
+        hostid: hostID,
+        venue,
+    };
+
+    return fetchWebRPC(endpoint, params);
+}
+
+export function showUser(projectURL, opaqueAuth, options) {
+    const endpoint = `${projectURL}${SHOW_USER_SUFFIX}`;
+    const params = {
+        opaque_auth: opaqueAuth,
+        format: 'xml',
+        ...options,
+    };
+
+    return fetchWebRPC(endpoint, params);
+}
--- src/webRPC/webRPC.js
+++ src/webRPC/webRPC.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import parser from 'fast-xml-parser';
 import queryString from 'query-string';
-import md5 from 'md5';
 
 export const METHOD_POST_DETAILS = 'user_posts';
 export const METHOD_THREAD_DETAILS = 'user_threads';
@@ -12,11 +11,6 @@ function constructURL(URL, params) {
 
 function toCORSProxyURL(URL, params) {
     return `https://cors.io/?${constructURL(URL, params)}`;
-}
-
-function createPasswdHash(password, email) {
-    // As documented in: https://boinc.berkeley.edu/trac/wiki/PasswordHash
-    return md5(password + email);
 }
 
 export function fetchWebRPC(projectEndpoint, params, requestOptions) {
@@ -34,69 +28,6 @@ export function fetchWebRPC(projectEndpoint, params, requestOptions) {
             reject(response);
         }
     }));
-}
-
-export function createAccount(projectURL, emailAddress, password, userName, options) {
-    const passwdHash = createPasswdHash(password, emailAddress);
-
-    const endpoint = `${projectURL}${CREATE_ACCOUNT_SUFFIX}`;
-    const params = {
-        email_addr: emailAddress,
-        passwd_hash: passwdHash,
-        user_name: userName,
-        ...options,
-    };
-
-    return fetchWebRPC(endpoint, params);
-}
-
-export function lookupAccount(projectURL, options) {
-    const endpoint = `${projectURL}${LOOKUP_ACCOUNT_SUFFIX}`;
-    return fetchWebRPC(endpoint, options);
-}
-
-export function getAccountInfo(projectURL, accountKey, opaqueAuth) {
-    const endpoint = `${projectURL}${GET_ACCOUNT_INFO_SUFFIX}`;
-    const params = {
-        account_key: accountKey,
-        opaque_auth: opaqueAuth,
-    };
-
-    return fetchWebRPC(endpoint, params);
-}
-
-export function setAccountInfo(projectURL, accountKey, opaqueAuth, options) {
-    const endpoint = `${projectURL}${SET_ACCOUNT_INFO_SUFFIX}`;
-    const params = {
-        account_key: accountKey,
-        opaque_auth: opaqueAuth,
-        ...options,
-    };
-
-    return fetchWebRPC(endpoint, params);
-}
-
-export function setHostInfo(projectURL, accountKey, opaqueAuth, hostID, venue) {
-    const endpoint = `${projectURL}${SET_HOST_INFO_SUFFIX}`;
-    const params = {
-        account_key: accountKey,
-        opaque_auth: opaqueAuth,
-        hostid: hostID,
-        venue,
-    };
-
-    return fetchWebRPC(endpoint, params);
-}
-
-export function showUser(projectURL, opaqueAuth, options) {
-    const endpoint = `${projectURL}${SHOW_USER_SUFFIX}`;
-    const params = {
-        opaque_auth: opaqueAuth,
-        format: 'xml',
-        ...options,
-    };
-
-    return fetchWebRPC(endpoint, params);
 }
 
 export function teamLookupByName(projectURL, teamName) {
