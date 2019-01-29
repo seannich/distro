cb3fbe73a4999f8e01772a06d96e6b9578575653
Joel Tio <joeltiojinhon@gmail.com>
2019-01-30 03:01:52 +0800

    Add collatz function into makeproject

--- server/images/makeproject/apps/collatz/1.0/browser/collatz.js
+++ server/images/makeproject/apps/collatz/1.0/browser/collatz.js
@@ -1,1 +1,39 @@
-console.log("Hello there, you are running collatz!");
+function jobMain(command_line) {
+    // Parse the command line args
+    const args = JSON.parse(command_line);
+
+    // Get the range of numbers from the arguments
+    const min = args['min'];
+    const max = args['max'];
+
+    function collatz(number) {
+        let currentNumber = number;
+        let pastNumbers = new Set();
+
+        while (currentNumber !== 1 && !pastNumbers.has(currentNumber)) {
+            if (currentNumber % 2 == 0) {
+                currentNumber /= 2;
+            } else {
+                currentNumber = currentNumber * 3 + 1;
+            }
+        }
+
+        return currentNumber === 1;
+    }
+
+    let resultString = '';
+
+    // Create results
+    for (let i=min; i<(max+1); i++) {
+        let collatzString = i.toString() + ',';
+        if (collatz(i)) {
+            collatzString += 'true';
+        } else {
+            collatzString += 'false';
+        }
+
+        resultString += collatzString + '\n';
+    }
+
+    return resultString;
+}
